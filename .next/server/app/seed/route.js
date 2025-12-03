edabe394d28737adcc4b545429c7dd4717db78e4
MARIANA PANTELIC <m.pantelic@dotsource.de>
2025-12-03 10:30:56 +0100

    2nd commit

---
+++ .next/server/app/seed/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/seed/route.js")
+R.c("server/chunks/node_modules_40aaad01._.js")
+R.c("server/chunks/[root-of-the-server]__f670a909._.js")
+R.m("[project]/.next-internal/server/app/seed/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/seed/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/seed/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
